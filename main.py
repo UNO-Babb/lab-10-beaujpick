928d98148836d225e4da1e7d40fd5805f02d3acc
beaujpick <beaujpick@gmail.com>
2025-11-17 05:35:29 +0000

    Done with lab

--- main.py
+++ main.py
@@ -2,3 +2,25 @@
 #Name:
 #Date:
 #Assignment:
+
+import cars
+import pandas
+import matplotlib.pyplot as plt
+car = cars.get_car()
+
+
+years = []
+mpgs = []
+for tire in car:
+    mpg = tire["Fuel Information"]["City mpg"]
+    year = tire["Identification"]["Year"]
+    years.append(year)
+    mpgs.append(mpg)
+    #print(make, year)
+
+df = pandas.DataFrame({"Year": years,
+                        "City mpg": mpg})
+print(df)
+df.plot(kind = 'scatter', x = 'Year', y = 'City mpg')
+plt.plot(years, mpgs, 'ro')
+plt.savefig("output")
